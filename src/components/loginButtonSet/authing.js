c1fed92efbf92a59c7fb0101fbb502cb2320fe3b
ivy <597055914@qq.com>
2019-02-15 17:56:23 +0800

    Change Authing Id

--- src/components/loginButtonSet/authing.js
+++ src/components/loginButtonSet/authing.js
@@ -5,17 +5,13 @@ import type { ButtonProps } from './';
 export class AuthingLoginForm extends React.Component<Props> {
   componentDidMount() {
     const loginForm = new AuthingForm({
-      clientId: '5c344f102e450b000170190a',
-      secret: '03bb8b2fca823137c7dec63fd0029fc2',
+      clientId: '5c668c712e450b00017af455',
+      secret: 'adafbe8412cb43293f4f739a5e9ef709',
       mountId: 'login-form',
       title: 'Spectrum',
       hideUP: true,
       logo:
         'https://spectrum.imgix.net/communities/-Kh6RfPYjmSaIWbkck8i/hgrta7Gv1-apple-icon-144x144-precomposed.png?w=256&h=256&expires=1550016000000&ixlib=js-1.2.0&s=b15a590ba2c5cea7cc32bf2b0abe5095',
-    });
-
-    loginForm.on('login', function(user) {
-      console.log(user);
     });
   }
 
